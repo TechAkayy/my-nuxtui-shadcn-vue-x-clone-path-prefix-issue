e222b71e808ef676c0413b49bff13b5ddbdc71b4
Akayy <techakayy@gmail.com>
2023-11-08 00:44:45 +1100

    first commit

---
+++ utils/index.ts
@@ -0,0 +1,26 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function classNames(...classes: any[string]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export const kebabCase = (str: string) => {
+  return str
+    ?.match(
+      /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g,
+    )
+    ?.map((x) => x.toLowerCase())
+    ?.join('-')
+}
+
+export const omit = (obj: object, keys: string[]) => {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => !keys.includes(key)),
+  )
+}
